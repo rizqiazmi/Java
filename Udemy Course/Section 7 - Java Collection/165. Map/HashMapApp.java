140e5aaa7a768058f8a010813338de687e602a34
mac <mac@macs-MacBook-Air.local>
2023-02-13 08:17:35 +0700

    Java

---
+++ Udemy Course/Section 7 - Java Collection/165. Map/HashMapApp.java
@@ -0,0 +1,18 @@
+package programmer.zaman.now.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapApp {
+    public static void main(String[] args) {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("name.first", "Eko");
+        map.put("name.middle", "Kurniawan");
+        map.put("name.last", "Kurniawan");
+
+        System.out.println(map.get("name.first"));
+        System.out.println(map.get("name.middle"));
+        System.out.println(map.get("name.last"));
+    }
+}
---
+++ Udemy Course/Section 7 - Java Collection/167. SortedMap/SortedMapApp.java
@@ -0,0 +1,30 @@
+package programmer.zaman.now.collection;
+
+import java.util.*;
+
+public class SortedMapApp {
+    public static void main(String[] args) {
+
+        Comparator<String> stringComparator = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1);
+            }
+        };
+
+        SortedMap<String, String> sortedMap = new TreeMap<>(stringComparator);
+
+        sortedMap.put("Eko", "Eko");
+        sortedMap.put("Budi", "Budi");
+        sortedMap.put("Joko", "Joko");
+
+        for (var key : sortedMap.keySet()){
+            System.out.println(key);
+        }
+
+        SortedMap<String, String> empty = Collections.emptySortedMap();
+        SortedMap<String, String> immutable = Collections.unmodifiableSortedMap(sortedMap);
+
+        // immutable.put("Nugraha", "Nugraha"); // error
+    }
+}
