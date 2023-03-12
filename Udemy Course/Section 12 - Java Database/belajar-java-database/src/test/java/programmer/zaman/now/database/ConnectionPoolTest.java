e276f6b4e9bc8a71f55787797832cb315e7dd859
rizqiazmi <riki.yzr96@gmail.com>
2023-03-12 10:10:28 +0700

    Java

---
+++ Udemy Course/Section 12 - Java Database/belajar-java-database/src/test/java/programmer/zaman/now/database/ConnectionPoolTest.java
@@ -0,0 +1,41 @@
+package programmer.zaman.now.database;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionPoolTest {
+
+    @Test
+    void testHikariCP() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/belajar_java_database");
+        config.setUsername("root");
+        config.setPassword("");
+
+        // konfigurasi pool
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(5);
+        config.setIdleTimeout(60_000);
+        config.setMaxLifetime(10 * 60_000);
+
+        try{
+            HikariDataSource dataSource = new HikariDataSource(config);
+            Connection connection = dataSource.getConnection();
+            connection.close();
+            connection.close();
+        } catch (SQLException exception) {
+            Assertions.fail(exception);
+        }
+    }
+
+    @Test
+    void testUtil() throws SQLException {
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+    }
+}
